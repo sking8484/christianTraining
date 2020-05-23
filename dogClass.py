14844f7563e368b8eefbbb4749aeb9293aa538b1
Darst King <sking8484@gmail.com>
2020-05-23 01:56:05 -0700

    function homewor

--- dogClass.py
+++ dogClass.py
@@ -1,7 +1,5 @@
-
-#Please create a dog class that has two parameters 
-	1.) It's noise is bark
-	2.) It's hair color is _____
-#Please create a function that makes this dog bark
-
-
+"""
+#Please create a dog function that takes in two parameters :
+	1.) It's noise (i.e. bark, moan, groan)
+	2.) It's hair color
+"""
